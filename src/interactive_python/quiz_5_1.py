8768f8363bd380085d82862fefcb1840a2b6b83b
Vasily Vlasov <vasily.vlasov@gmail.com>
2013-05-28 02:41:59 +0400

    Data Analysis: MapReduce Quiz

---
+++ src/interactive_python/quiz_5_1.py
@@ -0,0 +1,153 @@
+__author__ = 'vvlasov'
+
+
+class BankAccount:
+    def __init__(self, initial_balance):
+        """Creates an account with the given balance."""
+        self.balance = initial_balance
+        self.fees = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        if self.balance < 0:
+            self.balance -= 5
+            self.fees += 5
+
+    def get_balance(self):
+        return self.balance
+
+    def get_fees(self):
+        return self.fees
+
+
+my_account = BankAccount(10)
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.withdraw(15)
+my_account.deposit(20)
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(30)
+my_account.withdraw(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(50)
+my_account.deposit(30)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.deposit(20)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25)
+my_account.withdraw(5)
+my_account.deposit(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(10)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.deposit(30)
+my_account.withdraw(25)
+my_account.withdraw(10)
+my_account.deposit(20)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5)
+my_account.withdraw(15)
+my_account.deposit(10)
+my_account.withdraw(5)
+print my_account.get_balance(), my_account.get_fees()
+
+account1 = BankAccount(20)
+account1.deposit(10)
+account2 = BankAccount(10)
+account2.deposit(10)
+account2.withdraw(50)
+account1.withdraw(15)
+account1.withdraw(10)
+account2.deposit(30)
+account2.withdraw(15)
+account1.deposit(5)
+account1.withdraw(10)
+account2.withdraw(10)
+account2.deposit(25)
+account2.withdraw(15)
+account1.deposit(10)
+account1.withdraw(50)
+account2.deposit(25)
+account2.deposit(25)
+account1.deposit(30)
+account2.deposit(10)
+account1.withdraw(15)
+account2.withdraw(10)
+account1.withdraw(10)
+account2.deposit(15)
+account2.deposit(10)
+account2.withdraw(15)
+account1.deposit(15)
+account1.withdraw(20)
+account2.withdraw(10)
+account2.deposit(5)
+account2.withdraw(10)
+account1.deposit(10)
+account1.deposit(20)
+account2.withdraw(10)
+account2.deposit(5)
+account1.withdraw(15)
+account1.withdraw(20)
+account1.deposit(5)
+account2.deposit(10)
+account2.deposit(15)
+account2.deposit(20)
+account1.withdraw(15)
+account2.deposit(10)
+account1.deposit(25)
+account1.deposit(15)
+account1.deposit(10)
+account1.withdraw(10)
+account1.deposit(10)
+account2.deposit(20)
+account2.withdraw(15)
+account1.withdraw(20)
+account1.deposit(5)
+account1.deposit(10)
+account2.withdraw(20)
+print account1.get_balance(), account1.get_fees(), account2.get_balance(), account2.get_fees()
+
+primes = []
+numbers = range(2, 1000)
+for number in numbers:
+    is_prime = True
+    for prime in primes:
+        if number % prime == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(number)
+
+print len(primes)
+
+
+
+# simulation
+slow = 1000
+fast = 1
+
+year = 0
+while slow > fast:
+    slow *= 1.2
+    fast *= 1.4
+    year += 1
+
+print year
